bed5230be7d80b1c3fa2bb0c1ed86f082daf25a6
Duy Doan <cuibaponline08>
2016-04-11 08:40:48 +0700

    Init JSDemo

--- JSDemo/build/web/index.jsp
+++ JSDemo/build/web/index.jsp
@@ -1,6 +1,6 @@
 <%-- 
     Document   : index
-    Created on : Apr 6, 2016, 4:40:04 PM
+    Created on : Apr 8, 2016, 4:31:19 PM
     Author     : cuiba
 --%>
 
@@ -13,32 +13,37 @@
         <script src="js/main.js"></script>
     </head>
     <body>
-        <h1>User registration</h1>
-        <table>
-            <tr>
-                <td>Name: </td>
-                <td>
-                    <input type="text"/>
-                </td>
+        <h1>Hello World!</h1>
+        
+        <table border="1">
+            <tr id="tr1">
+                <td>aaaa</td>
+                <td>bbbb</td>
             </tr>
-            <tr>
-                <td>Email </td>
-                <td>
-                    <input id="email" type="text" />
-                </td>
+            <tr id="tr2">
+                <td>aaaa</td>
+                <td>bbbb</td>
             </tr>
-            <tr>
-                <td>Address: </td>
-                <td>
-                    <textarea></textarea>
-                </td>
+            <tr id="tr3">
+                <td>aaaa</td>
+                <td>bbbb</td>
             </tr>
-            <tr>
-                <td></td>
-                <td>
-                    <button onclick="demo()">Submit</button>
-                </td>
+            <tr id="tr4">
+                <td>aaaa</td>
+                <td>bbbb</td>
             </tr>
         </table>
+        <form>
+            <h2>Visibility</h2>
+            <input type="button" name="V1" onclick="changeVisibility('tr1')" />
+            <input type="button" name="V2" onclick="changeVisibility('tr2')" />
+            <input type="button" name="V3" onclick="changeVisibility('tr3')" />
+            <input type="button" name="V4" onclick="changeVisibility('tr4')" />
+            <h2>Display</h2>
+            <input type="button" name="V1" onclick="changeDisplay('tr1')" />
+            <input type="button" name="V2" onclick="changeDisplay('tr2')" />
+            <input type="button" name="V3" onclick="changeDisplay('tr3')" />
+            <input type="button" name="V4" onclick="changeDisplay('tr4')" />
+        </form>
     </body>
 </html>
--- JSDemo/build/web/js/main.js
+++ JSDemo/build/web/js/main.js
@@ -7,21 +7,85 @@
 
 /* global undefine */
 
+function changeVisibility(tr) {
+    if (document.getElementById(tr).style.visibility === "hidden") {
+        document.getElementById(tr).style.visibility = "visible";
+    } else {
+        document.getElementById(tr).style.visibility = "hidden";
+    }
+}
+
+function changeDisplay(tr){
+    if (document.getElementById(tr).style.display === "none") {
+        document.getElementById(tr).style.display = "block";
+    } else {
+        document.getElementById(tr).style.display = "none";
+    }
+}
+
+
+function changeSkin(newSkin) {
+    this.skin = newSkin;
+}
+
+function changeOnwer(newOnwer) {
+    this.owner = newOnwer;
+}
+
+function showDog() {
+    document.writeln("Kind: " + this.kind + " Name: " + this.name +
+            " Skin: " + this.skin + " Onwer: " + this.owner);
+}
+
+function dog() {
+    this.kind = "Ngao";
+    this.name = "Tây tạng";
+    this.skin = "Tử";
+    this.owner = ["D", "U", "Y"];
+
+    this.changeOnwer = changeOnwer;
+    this.changeSkin = changeSkin;
+    this.showDog = showDog;
+}
+
+
+
+function outputArray(intro, array) {
+    for (var i in array) {
+        for (var j in array[i]) {
+            for (var k in array[i][j]) {
+                document.writeln("a[" + i + "][" + j + "][" + k + "] = " + array[i][j][k] + "</br>");
+            }
+        }
+    }
+}
+
+function changeValue(value) {
+    value = "safsaas";
+}
+
+function compare1(value1, value2) {
+    return parseInt(value1) - parseInt(value2);
+}
+
+function compare2(value1, value2) {
+    return parseInt(value2) - parseInt(value1);
+}
+
 window.onload = function () {
     var el = document.getElementsByTagName("h1")[0];
     el.style.color = "red";
 }
 
-function validate(){
+function validate() {
     var regexPatern = "/[\w%_.]+@[\w-.]+\.[\w]{2,n}/";
     var email = document.getElementById('email').value;
-    
+
     var regex = new RegExp(regexPatern);
     var isValid = regex.test(email);
     if (isValid) {
         alert("Right");
-    }
-    else{
+    } else {
         alert("Wrong");
     }
 }
@@ -31,29 +95,29 @@ function demo() {
 //    document.appendChild("<h2>ahihi đồ ngốk</h2>");
     var input = document.getElementsByTagName("input")[0];
     var textArea = document.getElementsByTagName("textarea")[0];
-    
+
     var value = "5";
     var nullValue = null;
     var undefineValue;
-    
+
     if (undefineValue > 4) {
         textArea.value = undefineValue;
     }
-    
+
 //    var newArray = [];
     var newArray = new Array();
     newArray.push("cat");
     newArray.push("dog");
     newArray.push("bird");
     newArray.push("lion");
-    
-    var newValue =  new Array();
+
+    var newValue = new Array();
     newValue["A"] = "Hello";
-    
+
     if (true) {
         textArea.value = newValue["A"];
     }
-    
+
     validate();
 //    
 //    if (validate()) {
